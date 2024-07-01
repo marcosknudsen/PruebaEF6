733b54de851af0d56eea5ac2249dab00d8c155ca
Marcos Knudsen <marcosknu14@gmail.com>
2024-07-01 19:53:50 -0300

    country controller

---
+++ PruebaEF6/Controllers/CountryController.cs
@@ -0,0 +1,61 @@
+﻿using PruebaEF6.Models;
+using PruebaEF6.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaEF6.Controllers
+{
+    public class CountryController : Controller
+    {
+        private readonly CountryRepository countryRepository = new CountryRepository();
+
+        public async Task<ActionResult> Index()
+        {
+            List<Country> countries= await countryRepository.GetCountries();
+            return View(countries);
+        }
+
+        public ActionResult Create()
+        {
+            Country country = new Country();
+            return View(country);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            Country country = await countryRepository.GetCountry(id);
+            return View(country);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            Country country = await countryRepository.GetCountry(id);
+            return View(country);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCountry(Country country)
+        {
+            await countryRepository.AddCountry(country);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> EditCountry(Country country)
+        {
+            await countryRepository.EditCountry(country);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DeleteCountry(int id)
+        {
+            await countryRepository.DeleteCountry(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
